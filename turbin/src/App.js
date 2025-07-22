6d2906794d8568197f27833082e286acca628dfa
cuongdtdg <cuong241632016@lms.utc.edu.vn>
2025-07-22 08:55:49 +0700

    vercec

--- turbin/src/App.js
+++ turbin/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 // ...existing code...
 import LoginPage from './pages/LoginPage.jsx';
